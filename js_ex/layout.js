57772fc446ecaa4a59c8e718230d3a8ff238c304
Ella0916 <88914293+Ella0916@users.noreply.github.com>
2021-10-02 17:48:16 +0900

    1002 수업필기 경진 수업 필기

--- js_ex/layout.js
+++ js_ex/layout.js
@@ -15,3 +15,31 @@ close.addEventListener("click", function () {
   gnb.classList.remove("on");
   close.classList.remove("on");
 });
+
+// slide 롤링 효과 주기
+
+// 시간의 개념이 포함된 반복 실행 함수
+// setInterval (함수, 시간)
+
+// 일정 시간이 지난 후 실행 함수
+// setTimeout (함수, 시간)
+
+// 시간 매개변수 값의 단위 ms : 1000ms = 1s
+
+let currentIndex = 0;
+let nextIndex = 1;
+const slide = document.querySelectorAll(".slide");
+
+console.log(slide);
+
+setInterval(function () {
+  if (nextIndex > 2) {
+    nextIndex = 0;
+  }
+
+  slide[currentIndex].classList.remove("active");
+  slide[nextIndex].classList.add("active");
+
+  currentIndex = nextIndex;
+  nextIndex++;
+}, 3000);
